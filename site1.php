f957b03c418f3ff7267d63a6ac7836872d70736e
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2020-12-17 12:53:36 +0100

    dark mode

---
+++ site1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>KSP Tester | Welcome</title>
+    </head>
+    <body>
+        <div id="bloc_page">
+            <h1>welcome to Ksp-tester</h1>
+           
+
+        </div>
+
+
+    </body>
+</html>
