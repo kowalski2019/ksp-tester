d161a4d1ac299ee9642d279e9faa3e7ef3a8ecc9
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2020-12-17 14:24:03 +0100

    ksp-tester menu add

--- site1.php
+++ site1.php
@@ -6,12 +6,20 @@
         <title>KSP Tester | Welcome</title>
     </head>
     <body>
-        <div id="bloc_page">
-            <h1>welcome to Ksp-tester</h1>
-           
-
-        </div>
-
-
+        <form action="homepage.php" method="post" enctype="multipart/form-data">
+            <div id="bloc_page">
+                <h1>welcome to Ksp-tester</h1>
+            
+            </div>
+            <p>
+                How can ksp-tester help you?<br />
+                <input type="radio" name="choice" value="self-test" id="self-test" /> <label for="self-test">Test your virtual machine with a self-written test</label><br />
+                <input type="radio" name="choice" value="certain-test" id="certain-test" checked /> <label for="certain-test">Test certain functionality of your virtual machine</label><br />
+                <input type="radio" name="choice" value="auto-test" id="auto-test"  /> <label for="certain-test">Evaluate your ninja virtual machine</label><br />
+            </p>
+            </p>
+                <input type="submit" value="OK" />
+            </p>
+        </form>
     </body>
 </html>
