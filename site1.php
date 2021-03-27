14871e4ef5103d688e0468e58582bb81a68219b8
Carelle Djuffo Nzangue <carelle.djuffo.nzangue@mni.thm.de>
2021-03-27 14:08:40 +0100

    added navbar and transparent box for homepage text

--- site1.php
+++ site1.php
@@ -4,6 +4,9 @@
           <meta charset="utf-8" />
           <link rel="stylesheet" href="style.css" />
           <title>KSP Tester | Powered by @csmk</title>
+          <!-- Latest compiled and minified CSS -->
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
   </head>
   <body>
   <fieldset id="menu2">
@@ -35,5 +38,13 @@
       <input type="submit" name="submit" id="button"/>
     </form>
   </fieldset>
+  <!-- jQuery library -->
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+                  <!-- Popper JS -->
+                  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+                  <!-- Latest compiled JavaScript -->
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </body>
 </html>
