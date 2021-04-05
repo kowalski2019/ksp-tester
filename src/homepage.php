a0836e5f9b6a19d5292ca37cd71b950a574b88cb
Carelle Djuffo Nzangue <carelle.djuffo.nzangue@mni.thm.de>
2021-04-05 15:46:23 +0200

    added help and contact content

--- src/homepage.php
+++ src/homepage.php
@@ -23,9 +23,6 @@
               <a class="nav-link" href="homepage.php"> </i>Home</a>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
-            <li class="nav-item ">
               <a class="nav-link" href="contact.php" >Contact</a>
             </li>
             <li class="nav-item ">
--- src/site3.php
+++ src/site3.php
@@ -20,9 +20,7 @@
             <li class="nav-item ">
               <a class="nav-link" href="homepage.php"> </i>Home</a>
             </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
+
             <li class="nav-item ">
               <a class="nav-link" href="contact.php" >Contact</a>
             </li>
